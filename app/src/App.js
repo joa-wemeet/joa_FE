dc106038efbfb0ac82387146f577eb3c54ea5cb5
master <haram040131@naver.com>
2025-01-07 21:39:38 +0900

    haram

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './main/app.css'; 
-import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const navigate = useNavigate();
-
   return (
     <div className="container">
       {/* 시선 추적 */}
-      <div className="card" onClick={() => navigate('/mainpage')}>
+      
+      <div className="card">
         <h2 className="title">시선 추적</h2>
         <div className="icon">
-          <img src="/main/eye.png" alt="Eye Tracking" />
+          <img src="/main/eye.png" alt="Sample" />
         </div>
         <p className="description">
           시선추적을 사용하시려면 <span className="link">여기</span>를 보고 깜빡여 주세요
@@ -19,11 +17,12 @@ function App() {
       </div>
 
       {/* 터치 모드 */}
-      <div className="card" onClick={() => navigate('/mainpage')}>
-        <h2 className="title">터치 모드</h2>
+      <div className="card">
+      <h2 className="title">터치 모드</h2>
         <div className="icon">
-          <img src="/main/tap.png" alt="Touch Mode" />
+          <img src="/main/tap.png" alt="Sample" />
         </div>
+        
         <p className="description">
           터치모드를 이용하시려면 <span className="link">여기</span>를 클릭해 주세요
         </p>
@@ -32,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
--- app/src/mainpage.js
+++ app/src/mainpage.js
@@ -7,61 +7,120 @@ function MainPage() {
   };
 
   return (
+    <div className="container">
     <div className="main-page">
       <img src="/main/긴급전화통합.jpeg" alt="Emergency Numbers" className="top-image"/>
-      <h2 className="service-title">발급을 원하시는 증명서를 선택하세요</h2>
-      <div className="service-section">
-        {/* 각 서비스 박스 */}
-        <div className="service-box" onClick={() => handleServiceClick('주민등록')}>
-          <span>주민등록</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">무료</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('지적, 토지, 건축')}>
-          <span>지적, 토지, 건축</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">다음화면</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('차량')}>
-          <span>차량</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">다음화면</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('보건복지')}>
-          <span>보건복지</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">무료</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('농지대장')}>
-          <span>농지대장</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">다음화면</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('가족관계등록부')}>
-          <span>가족관계등록</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">500원</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('제적부')}>
-          <span>제적부</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">500원/300원</span>
-        </div>
-        <div className="service-box" onClick={() => handleServiceClick('병적증명서')}>
-          <span>병적증명서</span>
-          <div className="service-divider"></div>
-          <span className="line">--------------</span>
-          <span className="service-free">무료</span>
-        </div>
-        {/* 추가적인 서비스 박스 구현 */}
+      <h2 className="service-title" style={{ color: '#2e3d86' }}>발급을 원하시는 <span style={{ color: '#d94925' }}>
+      증명서를 선택</span>하세요</h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '15px', justifyContent: 'center' }}>
+        {[
+          { label: '주민등록', free: true },
+          { label: '지적, 토지, 건축', free: false },
+          { label: '차량', free: false },
+          { label: '보건복지', free: true },
+          { label: '농지대장', free: false },
+          { label: '가족관계등록부', price: '500원' },
+          { label: '제적부', price: '500원 / 300원' },
+          { label: '병적증명서', free: true },
+          { label: '지방세', free: false },
+          { label: '어선원부', price: '800원' },
+          { label: '교육제증명', free: true },
+          { label: '국세증명', free: true },
+          { label: '건강보험', free: true },
+          { label: '고용, 산재보험', free: true },
+          { label: '여권', free: true },
+          { label: '국민연금', free: true },
+        ].map((button, index) => (
+          <button
+            key={index}
+            style={{
+              width: '250px',
+              height: '130  px',
+              padding: '10px 20px',
+              border: '1px solid #ccc',
+              borderRadius: '5px',
+              backgroundColor: '#2e3d86',
+              cursor: 'pointer',
+              textAlign: 'center',
+              borderRadius: '15px',
+              fontSize: '1.7em',
+              color: '#FFFFFF'
+            }}
+          >
+            {button.label}
+            <hr
+            style={{
+              border: 'none',
+              borderTop: '2px dashed #999',
+              
+            }}
+          />
+            
+            {button.free ? '무료' : button.price || '다음화면'}
+          </button>
+        ))}
       </div>
+
+      {/* Bottom Buttons */}
+      <div style={{ display: 'flex', justifyContent: 'space-around', gap: '20px', margintop: '20px'}}>
+        <button
+          style={{
+            backgroundColor: '#568a35',
+            border: 'none',
+            borderRadius: '50%',
+            width: '120px',
+            height: '120px',
+            cursor: 'pointer',
+            fontSize: '18px',
+            color: '#FFFFFF',
+          }}
+        >
+          설치장소 안내
+        </button>
+        <button
+          style={{
+            backgroundColor: '#568a35',
+            border: 'none',
+            borderRadius: '50%',
+            width: '120px',
+            height: '120px',
+            cursor: 'pointer',
+            fontSize: '20px',
+            color: '#FFFFFF',
+          }}
+        >
+          서비스 시간
+        </button>
+        <button
+          style={{
+            backgroundColor: '#fffa5a',
+            border: 'none',
+            borderRadius: '10%',
+            width: '280px',
+            height: '120px',
+            cursor: 'pointer',
+            fontSize: '25px',
+            color: '#000000',
+          }}
+        >
+          증명서 검색
+        </button>
+        <button
+          style={{
+            backgroundColor: '#fffa5a',
+            border: 'none',
+            borderRadius: '10%',
+            width: '280px',
+            height: '120px',
+            cursor: 'pointer',
+            fontSize: '25px',
+            color: '#000000',
+          }}
+        >
+          화면전환<br /> (큰 글씨)
+        </button>
+      </div>
+    </div>
     </div>
   );
 }
